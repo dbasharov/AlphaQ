9bd4bd3c2b34167a12a5ae3b0ebff8b1c1787600
dbasharov <denis@skible.com>
2018-08-14 12:24:58 +0300

    Added sticky navigation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,94 +1,72 @@
-
-
-
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-    var that = this;
-    var delta = 150 - Math.random() * 100;
-    // var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
+class TxtType {
+    constructor(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
         this.isDeleting = false;
-        this.loopNum++;
-        delta = 100;
-        // delta = 500;
+
+        this.tick();
     }
 
-    setTimeout(function() {
-        that.tick();
-    }, delta);
-};
+    tick() {
+        const i = this.loopNum % this.toRotate.length;
+        const fullTxt = this.toRotate[i];
 
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = `<span class='wrap'>${this.txt}</span>`;
+
+        let delta = 150 - Math.random() * 100;
+        // var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) {
+            delta /= 2;
+        }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 100;
+            // delta = 500;
+        }
+
+        setTimeout(this.tick.bind(this), delta);
+    }
+}
+
+window.addEventListener('load', () => {
+    const parallaxDiv = document.querySelector('.parallax');
+    parallaxDiv.addEventListener('scroll', () => {
+        console.log($(parallaxDiv).scrollTop());
+        if ($(parallaxDiv).scrollTop() >= 250) {
+            $('nav').addClass('sticky');
+
+            // $('nav div').addClass('visible-title');
+        }
+        else {
+            $('nav').removeClass('sticky');
+            // $('nav div').removeClass('visible-title');
+        }
+
+    });
+
+    const elements = document.getElementsByClassName('typewrite');
+
+    for (let i = 0; i < elements.length; i++) {
+        const toRotate = elements[i].getAttribute('data-type');
+        const period = elements[i].getAttribute('data-period');
         if (toRotate) {
             new TxtType(elements[i], JSON.parse(toRotate), period);
         }
     }
 
-};
-
-
-
-//
-//
-//
-
-
-
-/*
-$(document).ready(function(){
-    $(window).scroll(function(){
-        alert(1);
-    });
 });
-
-
-$(window).scroll(function(){
-    alert(1);
-});
-
-*/
-
-/*
-
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 50) {
-        $('nav').addClass('fixed-header');
-        $('nav div').addClass('visible-title');
-    }
-    else {
-        $('nav').removeClass('fixed-header');
-        $('nav div').removeClass('visible-title');
-    }
-});
-
-*/
