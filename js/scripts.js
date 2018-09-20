f7983d9bea3d497ee9828805a5a4633b86438bc2
dbasharov <denis@skible.com>
2018-09-20 14:10:31 +0300

    anchor links added

--- js/scripts.js
+++ js/scripts.js
@@ -89,8 +89,6 @@ window.addEventListener('load', () => {
         }
 
 
-
-
     });
 
     const elements = document.getElementsByClassName('typewrite');
@@ -103,5 +101,77 @@ window.addEventListener('load', () => {
         }
     }
 
+
+
+
+    $(document).ready(function(){
+        $(".nav-header").on("click","a", function (event) {
+            event.preventDefault();
+            let id  = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('.parallax').stop().animate({scrollTop: top}, 900);
+        });
+    });
+
+
+
+
+/*
+
+    let linkNav = document.querySelectorAll('[href^="#"]'), //выбираем все ссылки к якорю на странице
+        V = 0.2;  // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
+
+    for (let i = 0; i < linkNav.length; i++) {
+        linkNav[i].addEventListener('click', function(e) { //по клику на ссылку
+            e.preventDefault(); //отменяем стандартное поведение
+            const w = window.pageYOffset,  // производим прокрутку
+                hash = this.href.replace(/[^#]*(.*)/, '$1');  // к id элемента, к которому нужно перейти
+            let t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
+                start = null;
+            requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
+            function step(time) {
+                if (start === null) start = time;
+                let progress = time - start,
+                    r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+                window.scrollTo(0,r);
+                if (r != w + t) {
+                    requestAnimationFrame(step)
+                } else {
+                    location.hash = hash  // URL с хэшем
+                }
+            }
+        }, false);
+    };
+
+
+*/
+
+
+/*
+
+    function runScroll() {
+        scrollTo(document.querySelector(".section-we-help"), 0, 900);
+    }
+    let scrollme;
+    scrollme = document.querySelector("#scrollmeHome");
+    scrollme.addEventListener("click",runScroll,false);
+
+    function scrollTo(element, to, duration) {
+        if (duration <= 0) return;
+        let difference = to - element.scrollTop;
+        let perTick = difference / duration * 10;
+
+        setTimeout(function() {
+            element.scrollTop = element.scrollTop + perTick;
+            if (element.scrollTop == to) return;
+            scrollTo(element, to, duration - 10);
+        }, 10);
+    }
+
+*/
+
+
+
 });
+
 
